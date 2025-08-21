7c82a1ef85c3dd246e386a654e8b190d0d652cb7
Ilsu Irmese <ilsu.irmese@gmail.com>
2025-08-21 13:29:21 +0300

    Update top-10-counts.py

--- scripts/scripts-english-nlp/top-10-counts.py
+++ scripts/scripts-english-nlp/top-10-counts.py
@@ -2,11 +2,11 @@ import pandas as pd
 
 ## MERGE DFS WITH ALL MENTIONS ##
 
-excel_business = "C:/Users/Ilsu/Desktop/School/Thesis/Project/stats/english-stats/all-mentions/all-mentions-english-business.xlsx"
-excel_entertainment = "C:/Users/Ilsu/Desktop/School/Thesis/Project/stats/english-stats/all-mentions/all-mentions-english-entertainment.xlsx"
-excel_politics = "C:/Users/Ilsu/Desktop/School/Thesis/Project/stats/english-stats/all-mentions/all-mentions-english-politics.xlsx"
-excel_sport = "C:/Users/Ilsu/Desktop/School/Thesis/Project/stats/english-stats/all-mentions/all-mentions-english-sport.xlsx"
-excel_tech = "C:/Users/Ilsu/Desktop/School/Thesis/Project/stats/english-stats/all-mentions/all-mentions-english-tech.xlsx"
+excel_business = ""
+excel_entertainment = ""
+excel_politics = ""
+excel_sport = ""
+excel_tech = ""
 
 df_business = pd.read_excel(excel_business)
 df_entertainment = pd.read_excel(excel_entertainment)
@@ -16,7 +16,6 @@ df_tech = pd.read_excel(excel_tech)
 
 bbc_all_mentions = pd.concat([df_business, df_entertainment, df_politics, df_sport, df_tech], ignore_index=True)
 
-#%%
 ## TOP 100 STATS ##
 def top_repeated_names(df, top_n=150):
     # Count how many times each name appears
@@ -33,7 +32,7 @@ def top_repeated_names(df, top_n=150):
     return result.head(top_n)
 top_150_bbc = top_repeated_names(bbc_all_mentions)
 top_150_bbc.to_excel("top_150_bbc.xlsx", index=False, engine='openpyxl')
-#%%
+
 def top_10_by_gender(df):
     # First, drop rows where gender is not male or female
     filtered = df[df['gender'].isin(['male', 'female'])]
@@ -55,16 +54,16 @@ def top_10_by_gender(df):
     # Combine into one DataFrame
     top_combined = pd.concat([top_women, top_men], ignore_index=True)
     return top_combined
-#%%
+
 top_10_by_gender_bbc = top_10_by_gender(bbc_all_mentions)
 top_10_by_gender_bbc.to_excel("top_10_by_gender_bbc.xlsx", index=False, engine='openpyxl')
 #%%
 ## TURKISH ##
-excel_culture_tr = "C:/Users/Ilsu/Desktop/School/Thesis/Project/stats/turkish-stats/all-mentions/all-mentions-turkish-clean-culture.xlsx"
-excel_economy_tr = "C:/Users/Ilsu/Desktop/School/Thesis/Project/stats/turkish-stats/all-mentions/all-mentions-turkish-clean-economy.xlsx"
-excel_politics_tr = "C:/Users/Ilsu/Desktop/School/Thesis/Project/stats/turkish-stats/all-mentions/all-mentions-turkish-clean-politics.xlsx"
-excel_sport_tr = "C:/Users/Ilsu/Desktop/School/Thesis/Project/stats/turkish-stats/all-mentions/all-mentions-turkish-clean-sport.xlsx"
-excel_tech_tr = "C:/Users/Ilsu/Desktop/School/Thesis/Project/stats/turkish-stats/all-mentions/all-mentions-turkish-clean-tech.xlsx"
+excel_culture_tr = ""
+excel_economy_tr = ""
+excel_politics_tr = ""
+excel_sport_tr = ""
+excel_tech_tr = ""
 
 df_culture_tr = pd.read_excel(excel_culture_tr)
 df_economy_tr = pd.read_excel(excel_culture_tr)
@@ -76,7 +75,7 @@ top_150_milliyet = top_repeated_names(milliyet_all_mentions)
 top_150_milliyet.to_excel("top_150_milliyet.xlsx", index=False, engine='openpyxl')
 top_10_by_gender_milliyet = top_10_by_gender(milliyet_all_mentions)
 top_10_by_gender_milliyet.to_excel("top_10_by_gender_milliyet.xlsx", index=False, engine='openpyxl')
-# %%
 top_10_by_gender_milliyet = top_10_by_gender(milliyet_all_mentions)
 top_10_by_gender_milliyet.to_excel("top_10_by_gender_milliyet.xlsx", index=False, engine='openpyxl')
-# %%
+
+
